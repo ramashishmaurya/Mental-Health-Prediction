7e7c310915d8772695b718e8f011ede3e535d236
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-16 08:46:05 +0530

    awe are learning how mlflow can implement in projects

---
+++ main.py
@@ -0,0 +1,28 @@
+import mlflow
+
+mlflow.set_experiment('mentalhealth') 
+
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Load the Iris dataset
+X, y = datasets.load_iris(return_X_y=True)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define the model hyperparameters
+params = {
+    "solver": "lbfgs",
+    "max_iter": 1000,
+    "random_state": 8888,
+}
+
+mlflow.sklearn.autolog()
+
+lr = LogisticRegression(**params)
+lr.fit(X_train, y_train)
+
